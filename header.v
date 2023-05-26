a8bdb57f1516348822ccdec1f095a784344a2504
Julia Desmazes <julia.desmazes@gmail.com>
2023-05-26 07:34:06 -0700

    RTL : Added header module to decode the first payload of the axi stream and map the correct fields. Added supporting formal, failing as intended

--- header.v
+++ header.v
@@ -7,15 +7,15 @@ module header(
 	// session id, spread over 2 parts
 	output logic        sid_p0_v_o, 
 	output logic [63:0] sid_p0_o,
-	output logic        sid_p1_v_o.	 
-	output logic [15:0] sid_p1_o.
+	output logic        sid_p1_v_o,	 
+	output logic [15:0] sid_p1_o,
 	// sequence number	   
 	output logic        seq_num_p0_v_o,
 	output logic [47:0] seq_num_p0_o,
 	output logic        seq_num_p1_v_o,
 	output logic [15:0] seq_num_p1_o,
 	// message cnt
-	output logic        msg_cnt_v_o
+	output logic        msg_cnt_v_o,
 	output logic [15:0] msg_cnt_o 
 
 );
@@ -42,6 +42,27 @@ always_comb begin
 	// assert
 	// no overlapping header decode step
 	sva_h_v_i_onehot0 : assert ( $onehot0( h_v_f ));
+
+	// xcheck
+	// validity bit is never x
+	sva_xcheck_sid_p0_v_o : assert ( ~$isunknown( sid_p0_v_o ) );
+	sva_xcheck_sid_p1_v_o : assert ( ~$isunknown( sid_p1_v_o ) );
+	sva_xcheck_seq_num_p0_v_o : assert ( ~$isunknown( seq_num_p0_v_o ) );
+	sva_xcheck_seq_num_p1_v_o : assert ( ~$isunknown( seq_num_p1_v_o ) );
+	sva_xcheck_msg_cnt_v_o : assert ( ~$isunknown( msg_cnt_o ));
+	// if output is valid, data is known 
+	sva_xcheck_sid_p0_o : assert ( ~sid_p0_v_o | ( sid_p0_v_o & ~$isunknown( sid_p0_o )));
+	sva_xcheck_sid_p1_o : assert ( ~sid_p1_v_o | ( sid_p1_v_o & ~$isunknown( sid_p1_o )));
+	sva_xcheck_seq_num_p0_o : assert ( ~seq_num_p0_v_o | ( seq_num_p0_v_o & ~$isunknown( seq_num_p0_o )));
+	sva_xcheck_seq_num_p1_o : assert ( ~seq_num_p1_v_o | ( seq_num_p1_v_o & ~$isunknown( seq_num_p1_o )));
+	sva_xcheck_msg_cnt_o : assert ( ~msg_cnt_v_o | ( msg_cnt_v_o & ~$isunknown( msg_cnt_o )));
+
+	// cover
+	c_invalid_i : cover ( ~|h_v_f );
+	c_h0_v_i : cover ( h0_v_i );
+	c_h1_v_i : cover ( h1_v_i );
+	c_h2_v_i : cover ( h2_v_i );
+
 end
 `endif
 endmodule
--- top.v
+++ top.v
@@ -16,38 +16,12 @@ module top #(
 	
 	output                 upd_axis_tready_o,
 );
-// axi payload buffering ( for timing )
-reg [AXI_DATA_W-1:0]  upd_axis_tdata_q;
-reg [AXI_KEEP_W-1:0]  upd_axis_tkeep_q;
-reg                   upd_axis_tvalid_q;
-reg                   upd_axis_tlast_q;
-reg                   upd_axis_tuser_q;
-wire [AXI_DATA_W-1:0] upd_axis_tdata_next;
-wire [AXI_KEEP_W-1:0] upd_axis_tkeep_next;
-wire                  upd_axis_tvalid_next;
-wire                  upd_axis_tlast_next;
-wire                  upd_axis_tuser_next;
 
-always @(posedge clk) 
-begin
-	if ( ~nreset ) begin
-		upd_axis_tvalid_q  <= 1'b0;
-	end else begin
-		upd_axis_tdata_q   <= upd_axis_tdata_next; 	
-		upd_axis_tkeep_q   <= upd_axis_tkeep_next;
-		upd_axis_tvalid_q  <= upd_axis_tvalid_next;
-		upd_axis_tlast_q   <= upd_axis_tlast_next;
-		upd_axis_tuser_q   <= upd_axis_tuser_next;
-	end
-end
-
-// 
-
-// fsm
+// FSM
 reg  fsm_invalid_q;
 wire fsm_invalid_next;
 // header 
-// pX  : header is received over multiple cycles 
+// hX  : header is received over multiple cycles 
 // msg : during last cycle part of the packed is the begining of the message
 reg  fsm_header_h0_q;
 reg  fsm_header_h1_q;
@@ -70,6 +44,56 @@ wire fsm_msg_m0_overlap_next;
 wire fsm_msg_m1_next;
 wire fsm_msg_m1_overlap_next;
 
+// AXI 
+reg [AXI_DATA_W-1:0]  upd_axis_tdata_q;
+reg [AXI_KEEP_W-1:0]  upd_axis_tkeep_q;
+reg                   upd_axis_tvalid_q;
+reg                   upd_axis_tlast_q;
+reg                   upd_axis_tuser_q;
+wire [AXI_DATA_W-1:0] upd_axis_tdata_next;
+wire [AXI_KEEP_W-1:0] upd_axis_tkeep_next;
+wire                  upd_axis_tvalid_next;
+wire                  upd_axis_tlast_next;
+wire                  upd_axis_tuser_next;
+
+// axi payload buffering ( for timing, might replace with clk domain crossing )
+always @(posedge clk) 
+begin
+	if ( ~nreset ) begin
+		upd_axis_tvalid_q  <= 1'b0;
+	end else begin
+		upd_axis_tdata_q   <= upd_axis_tdata_next; 	
+		upd_axis_tkeep_q   <= upd_axis_tkeep_next;
+		upd_axis_tvalid_q  <= upd_axis_tvalid_next;
+		upd_axis_tlast_q   <= upd_axis_tlast_next;
+		upd_axis_tuser_q   <= upd_axis_tuser_next;
+	end
+end
+// 
+// Header
+header m_header(
+	.data_i(upd_axis_tdata_q),
+	.h0_v_i(fsm_header_h0_q),
+	.h1_v_i(fsm_header_h1_q),
+	.h1_v_i(fsm_header_h2_msg_q),
+
+	.sid_p0_v_o(), 
+	.sid_p0_o(),
+	.sid_p1_v_o(),
+	.sid_p1_o(),
+	.seq_num_p0_v_o(),
+	.seq_num_p0_o(),
+	.seq_num_p1_v_o(),
+	.seq_num_p1_o(),
+	.msg_cnt_v_o(),
+	.msg_cnt_o ()
+
+);
+
+
+// fsm
+
+
 
 assign fsm_header_h0_next     = fsm_invalid_q & upd_axis_tvalid_i;
 assign fsm_header_h1_next     = fsm_header_h0_q;
@@ -77,7 +101,6 @@ assign fsm_header_h2_msg_next = fsm_header_h1_q;
 
 assign fsm_msg_m0_next = fsm_header_h2_msg_q; // default state
  
-assign fsm_ms
 always @(posedge clk)
 begin
 	if ( ~nreset ) begin
@@ -117,10 +140,19 @@ assign fsm_f = {
 initial begin
 	// assume
 	a_reset : assume ( ~nreset );
+	
 end
 
 always @(posedge clk) begin
 	if ( nreset ) begin
+		// assume
+		// AXI valid is never X
+		a_axi_tvalid_know : assume ( ~$isunknown( upd_axis_tvalid_i ));
+		// AXI doesn't drive X's when valid
+		a_axi_valid_tdata_known : assume( upd_axis_tvalid_i | ( upd_axis_tvalid_i &  ~$isunknown( upd_axis_tdata_i )));
+		a_axi_valid_tkeep_known : assume( upd_axis_tvalid_i | ( upd_axis_tvalid_i &  ~$isunknown( upd_axis_tkeep_i )));
+		a_axi_valid_tlast_known : assume( upd_axis_tvalid_i | ( upd_axis_tvalid_i &  ~$isunknown( upd_axis_tlast_i )));
+		a_axi_valid_tuser_known : assume( upd_axis_tvalid_i | ( upd_axis_tvalid_i &  ~$isunknown( upd_axis_tuser_i )));
 		// fsm
 		sva_fsm_onehot : assert( $onehot( fsm_f )); 
 	end
